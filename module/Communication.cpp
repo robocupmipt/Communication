0d66b64ad12b7186547e2f4974d13702cbcd8b01
molvlad <molvlad@frtk.ru>
2019-03-10 16:43:56 +0300

    restoring order

--- module/Communication.cpp
+++ module/Communication.cpp
@@ -4,12 +4,11 @@
 #include <qi/log.hpp>
 #include <unistd.h>
 
-Communication::Communication(boost::shared_ptr<AL::ALBroker> broker,
-                   const std::string& name)
+Communication::Communication(boost::shared_ptr<AL::ALBroker> broker, const std::string& name)
   : AL::ALModule(broker, name), tts_(getParentBroker())
 {
   setReturn("boolean", "return true if it was succesfully");
-  BIND_METHOD(Communication::sendRobotState);
+  BIND_METHOD(sendRobotState);
 }
 
 Communication::~Communication()
@@ -20,71 +19,6 @@ Communication::~Communication()
 void Communication::init()
 {
 
-}
-
-/*
-std::string retState(RobotStates robotState);
-
-typedef enum RobotStates
-{
-	NotDefined,
-	Initial,
-	Ready,
-	Set,
-	Playing,
-	Penalized,
-	Finished
-} RobotStates;
-
-RobotStates curRobotState = NotDefined;
-std::string retState(RobotStates robotState)
-{
-	std::string ret;
-	switch(robotState)
-	{
-		case Initial:
-			ret = "Initial";
-			break;
-		case Ready:
-			ret = "Ready";
-			break;
-		case Set:
-			ret = "Set";
-			break;
-		case Playing:
-			ret = "Playing";
-			break;
-		case Penalized:
-			ret = "Penalized";
-			break;
-		case Finished:
-			ret = "Finished";
-			break;
-		case NotDefined:
-		default:
-			ret = "NotDefined";
-			break;
-	}
-
-	return ret;
-}
-*/
-
-bool Communication::sendRobotState()
-{
-	bool error = false;
-
-	/*
-	if(curRobotState == NotDefined)
-	{
-		std::cout << "Current state = NotDefined. Can't transmit state.\n";
-		return error;
-	}
-
-	std::string stringState = retState(ComState::curRobotState);
-	// here should be call for transmit state
-	std::cout << "Current state = " << stringState << " transmit succesfully.\n";
-	*/
 }
 
 void Communication::startModule()
--- module/Communication.h
+++ module/Communication.h
@@ -1,39 +1,9 @@
 #ifndef COMMUNICATION_H
-# define COMMUNICATION_H
+#define COMMUNICATION_H
 
 #include <iostream>
 #include <alcommon/albroker.h>
 #include <alproxies/altexttospeechproxy.h>
-
-/*
-#define SPL_STANDARD_MESSAGE_STRUCT_HEADER "SPL"
-#define SPL_STANDARD_MESSAGE_STRUCT_VERSION 7
-#define PLAYER_NUM 6
-#define TEAM_NUM 1
-#define SPL_STANDARD_MESSAGE_DATA_SIZE 474
-
-char header[4] = SPL_STANDARD_MESSAGE_STRUCT_HEADER;
-uint8_t version = SPL_STANDARD_MESSAGE_STRUCT_VERSION;
-uint8_t playerNum = PLAYER_NUM;
-uint8_t teamNum = TEAM_NUM;
-
-// number of bytes that is actually used by the data array
-  uint16_t numOfDataBytes = SPL_STANDARD_MESSAGE_DATA_SIZE;
-  // buffer for arbitrary data, teams do not need to send more than specified in numOfDataBytes
-  uint8_t data[SPL_STANDARD_MESSAGE_DATA_SIZE];
-
-typedef struct DataForMessage
-{
-	// position and orientation of robot
-	float pose[3];
-	// ball information
-	float ballAge;
-	// position of ball relative to the robot
-	float ball[2];
-	// 1 means that the robot is fallen, 0 means that the robot can play
-	uint8_t fallen;
-} DataForMessage;
-*/
 
 namespace AL
 {
@@ -57,14 +27,14 @@ public:
   // Start loop receive/sendState/receive
   void startReceiveLoop();
 
-  //--------- Functions to communicate with Game Controller
+  // Functions to communicate with Game Controller
 
   // Transmit data to GameController
   bool transmitToGC();
   // Receive data from GameController
   bool receiveFromGC();
 
-  //--------- Functions to communicate with other modules
+  // Functions to communicate with other modules
 
   // Send current robot state to Strategy module
   bool sendRobotState();
@@ -74,14 +44,10 @@ public:
 
   // Write ball position (should be called from CV module)
   void writeBallPosition();
-  // Function which prints the word given on parameters
-  void printWord(const std::string &word);
-  // Function which returns true
-  bool returnTrue();
 
   bool sayWord(const std::string &word);
 
-  //--------- Functions to communicate with other robots
+  // Functions to communicate with other robots
 
   // To be continued
 
