16663377e879b87ede5278a9d2f367252fea80b3
molvlad <molvlad@frtk.ru>
2019-03-28 21:34:50 +0300

    clean

--- include/queue_config.h
+++ include/queue_config.h
@@ -1,10 +1,1 @@
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<sys/ipc.h>
-#include<sys/msg.h>
-#include<fcntl.h>
-#include<errno.h>
-#include<stdio.h>
-#include<stdlib.h>
 
